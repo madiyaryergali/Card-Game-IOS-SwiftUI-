0746528629ed71323ce9dff42254bfc2d53a659b
madiyaryergali <58412260+madiyaryergali@users.noreply.github.com>
2022-06-20 23:01:37 +0600

    Add files via upload

---
+++ first/Shared/ContentView.swift
@@ -0,0 +1,99 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Madiyar Yergali on 08.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let fRand = Int.random(in: 2...14)
+    let f1Rand = Int.random(in: 2...14)
+    @State var playerCard = "card"
+    @State var cpuCard = "card"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    @State var firstEntrance = true
+    
+    var body: some View {
+        ZStack{
+            Image("background").ignoresSafeArea()
+        VStack {
+            Spacer()
+            
+            Image("logo")
+
+            Spacer()
+            
+            HStack {
+                Spacer()
+                Image(playerCard)
+                Spacer()
+                Image(cpuCard)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                
+              
+                //generate a random number between 2-14
+                let playerRand = Int.random(in: 2...14)
+                let cpuRand = Int.random(in: 2...14)
+                
+                
+                //Update the cards
+                playerCard = "card" + String(playerRand)
+                cpuCard = "card" + String(cpuRand)
+                
+                if (playerRand > cpuRand) {
+                    playerScore += 1
+                }
+                else if (playerRand < cpuRand){
+                    cpuScore += 1
+                }
+                //Update the score
+                
+            }, label: {
+                Image("dealbutton")
+            })
+            
+            
+            Spacer()
+            
+            HStack{
+                Spacer()
+                VStack{
+                    Text("Player").font(.headline)
+                        .foregroundColor(Color.white)
+                   
+                    Text(String(playerScore))
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+                
+                VStack{
+                    Text("CPU")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                   
+                    Text(String(cpuScore)).font(.largeTitle)
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+            }
+            Spacer()
+        }
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first/Shared/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  Shared
+//
+//  Created by Madiyar Yergali on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
